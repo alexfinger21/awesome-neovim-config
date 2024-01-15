97bff37e8bc8c0774f31a407cc0fc7e7edba694d
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2024-01-15 14:29:14 -0500

    Update nvim_tree.lua

--- after/plugin/nvim_tree.lua
+++ after/plugin/nvim_tree.lua
@@ -1,11 +1,7 @@
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- OR setup with some options
+--setup with some options
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
