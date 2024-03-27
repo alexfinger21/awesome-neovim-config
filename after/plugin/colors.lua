e246482c72d4376ee5cbec4ab9d62f9805006971
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2024-03-27 01:05:56 -0400

    Update colors.lua

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,7 @@
 function colorMyPencils(color)
     color = color or "catppuccin-latte"
     vim.cmd.colorscheme(color)
-    vim.api.nvim_command("highlight Normal guibg=#FDF6E3")  
+    vim.api.nvim_command("highlight Normal guibg=#fcf1d2")  
 
     vim.api.nvim_set_hl(1, "Normal", {bg = "none"})
     vim.api.nvim_set_hl(1, "NormalFloat", {bg = "none"})
