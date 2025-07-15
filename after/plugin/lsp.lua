e18e99304ca8e134e7c1af1e09353477cffde37b
alexfinger21 <cdx9933@gmail.com>
2025-07-15 14:24:54 -0400

    added some custom telescope stufff or going back and forth between config and prev cdir

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -31,14 +31,110 @@ cmp.setup({
   },
 })
 
+-- Vimspector
+vim.cmd([[
+nmap <F9> <cmd>call vimspector#Launch()<cr>
+nmap <F5> <cmd>call vimspector#StepOver()<cr>
+nmap <F8> <cmd>call vimspector#Reset()<cr>
+nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+nmap <F12> <cmd>call vimspector#StepOut()<cr>")
+nmap <F10> <cmd>call vimspector#StepInto()<cr>")
+]])
+
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
+
+vim.cmd([[
+let g:vimspector_sidebar_width = 85
+let g:vimspector_bottombar_height = 15
+let g:vimspector_terminal_maxwidth = 70
+]])
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
 vim.diagnostic.config({
   virtual_text = {
     prefix = '⚡',
     spacing = 2,
   },
-  signs = true,
-  underline = true,
-  update_in_insert = true,
-  severity_sort = true,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
 })
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {
+  bg = "#fff0f0", -- warm pale red
+  fg = "#990000", -- deep red
+})
+
+-- Warning: stronger yellow background
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", {
+  bg = "#fff6b0", -- bright pale yellow
+  fg = "#8a6d00", -- dark golden yellow
+})
+
+-- Info: warm creamy yellow
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", {
+  bg = "#fef7c3", -- soft vanilla
+  fg = "#5a5a00", -- olive/brown text
+})
+
+-- Hint: yellowish mint
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", {
+  bg = "#fdfdcc", -- light yellow-green
+  fg = "#3b5f00", -- earthy green
+})
+
+-- Open floating diagnostic message
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Show diagnostics in float" })
+
+-- Go to next diagnostic
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+
+-- Go to previous diagnostic
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+
+-- Set location list with diagnostics
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
+
 
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,26 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep String > ")})
+end)
+
+local old_path = vim.fn.expand('%:p:h')
+
+vim.keymap.set("n", "<leader>en", function()
+    if not string.find(vim.fn.expand("%:p:h"), vim.fn.stdpath("config")) then
+        old_path = vim.fn.expand('%:p:h')
+    end
+
+    require("telescope.builtin").find_files {
+        cwd = vim.fn.stdpath("config")
+    }
+end)
+
+vim.keymap.set("n", "<leader>eo", function()
+    require("telescope.builtin").find_files {
+        cwd = old_path
+    }
 end)
 
