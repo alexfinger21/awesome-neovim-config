74bbbe8902f6b8380b6dabb66730436adbf83e76
alexfinger21 <cdx9933@gmail.com>
2025-07-16 13:38:14 -0400

    mason stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -31,7 +31,7 @@ cmp.setup({
   },
 })
 
--- Vimspector
+-- vimspector
 vim.cmd([[
 nmap <F9> <cmd>call vimspector#Launch()<cr>
 nmap <F5> <cmd>call vimspector#StepOver()<cr>
@@ -41,13 +41,13 @@ nmap <F12> <cmd>call vimspector#StepOut()<cr>")
 nmap <F10> <cmd>call vimspector#StepInto()<cr>")
 ]])
 
---Set completeopt to have a better completion experience
+--set completeopt to have a better completion experience
 -- :help completeopt
 -- menuone: popup even when there's only one match
--- noinsert: Do not insert text until a selection is made
--- noselect: Do not select, force to select one from the menu
+-- noinsert: do not insert text until a selection is made
+-- noselect: do not select, force to select one from the menu
 -- shortness: avoid showing extra messages when using completion
--- updatetime: set updatetime for CursorHold
+-- updatetime: set updatetime for cursorhold
 vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
 vim.opt.shortmess = vim.opt.shortmess + { c = true}
 vim.api.nvim_set_option('updatetime', 300) 
@@ -58,10 +58,10 @@ let g:vimspector_bottombar_height = 15
 let g:vimspector_terminal_maxwidth = 70
 ]])
 
--- Fixed column for diagnostics to appear
--- Show autodiagnostic popup on cursor hover_range
--- Goto previous / next diagnostic warning / error 
--- Show inlay_hints more frequently 
+-- fixed column for diagnostics to appear
+-- show autodiagnostic popup on cursor hover_range
+-- goto previous / next diagnostic warning / error 
+-- show inlay_hints more frequently 
 vim.cmd([[
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
@@ -72,9 +72,9 @@ local rt = require("rust-tools")
 rt.setup({
   server = {
     on_attach = function(_, bufnr)
-      -- Hover actions
+      -- hover actions
       vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
+      -- code action groups
       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
@@ -107,34 +107,39 @@ vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {
   fg = "#990000", -- deep red
 })
 
--- Warning: stronger yellow background
+-- warning: stronger yellow background
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", {
   bg = "#fff6b0", -- bright pale yellow
   fg = "#8a6d00", -- dark golden yellow
 })
 
--- Info: warm creamy yellow
+-- info: warm creamy yellow
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", {
   bg = "#fef7c3", -- soft vanilla
   fg = "#5a5a00", -- olive/brown text
 })
 
--- Hint: yellowish mint
+-- hint: yellowish mint
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", {
   bg = "#fdfdcc", -- light yellow-green
   fg = "#3b5f00", -- earthy green
 })
 
--- Open floating diagnostic message
+-- open floating diagnostic message
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Show diagnostics in float" })
 
--- Go to next diagnostic
+-- go to next diagnostic
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
 
--- Go to previous diagnostic
+-- go to previous diagnostic
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
 
--- Set location list with diagnostics
+-- set location list with diagnostics
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
 
-
+-- turn off diagnostics
+vim.keymap.set("n", "<leader>dl", function ()
+    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0})) do
+        vim.lsp.buf_detach_client(0, client.id)
+    end
+end)
