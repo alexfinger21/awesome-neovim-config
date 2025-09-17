c94685ce87ce2341854b5798d4b7a4f3beac45ad
alexfinger21 <cdx9933@gmail.com>
2025-09-16 23:13:53 -0400

    vue js lsp stuff

--- lua/alexfinger21/init.lua
+++ lua/alexfinger21/init.lua
@@ -1,7 +1,14 @@
+local mason_registry = require("mason-registry")
+
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "clangd" }, -- add more servers as needed
+    ensure_installed = { "clangd", "vue_ls", "ts_ls" }, -- add more servers as needed
 })
+
+-- Find the Vue language server install path
+local vue_language_server_path =
+  mason_registry.get_package("vue-language-server"):get_install_path()
+  .. "/node_modules/@vue/language-server"
 
 -- capabilities from nvim-cmp (optional but recommended)
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -9,10 +16,31 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- configure clangd using lspconfig
 local lspconfig = require("lspconfig")
 lspconfig.clangd.setup({
-  capabilities = capabilities,
-  -- you can also add on_attach, cmd, etc. here
+    capabilities = capabilities,
+    -- you can also add on_attach, cmd, etc. here
 })
 
+lspconfig.ts_ls.setup({
+    capabilities = capabilities,
+    init_options = {
+        plugins = {
+            {
+                name = "@vue/typescript-plugin",
+                location = vue_language_server_path,
+                languages = { "vue" }
+            }
+        }
+    },
+    filetypes = {
+        "javascript", "javascriptreact", "javascript.jsx",
+        "typescript", "typescriptreact", "typescript.tsx",
+        "vue",
+    }
+})
+
+lspconfig.vue_ls.setup({
+    capabilities = capabilities
+})
 
 require("alexfinger21.remap")
 require("alexfinger21.set")
--- lua/alexfinger21/packer.lua
+++ lua/alexfinger21/packer.lua
@@ -1,68 +1,68 @@
- return require('packer').startup(function(use)
-  -- Packer manages itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+    -- Packer manages itself
+    use "wbthomason/packer.nvim"
 
-  -- Mason + LSP
-  use("williamboman/mason.nvim")
-  use("williamboman/mason-lspconfig.nvim")
-  use("neovim/nvim-lspconfig")
+    -- Mason + LSP
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
+    use("neovim/nvim-lspconfig")
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Telescope
+    use {
+        "nvim-telescope/telescope.nvim", tag = "0.1.5",
+        requires = { {"nvim-lua/plenary.nvim"} }
+    }
 
-  -- Theme
-  use({ "rebelot/kanagawa.nvim", as = "kanagawa", config = function() vim.cmd("colorscheme kanagawa-lotus") end })
+    -- Theme
+    use({ "rebelot/kanagawa.nvim", as = "kanagawa", config = function() vim.cmd("colorscheme kanagawa-lotus") end })
 
-  -- Treesitter
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate"
-  })
-  use("nvim-treesitter/playground")
+    -- Treesitter
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate"
+    })
+    use("nvim-treesitter/playground")
 
-  -- Git, navigation, utils
-  use("ThePrimeagen/harpoon")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
-  use("simrat39/rust-tools.nvim")
+    -- Git, navigation, utils
+    use("ThePrimeagen/harpoon")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("simrat39/rust-tools.nvim")
 
-  -- Completion
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-nvim-lua")
-  use("hrsh7th/cmp-nvim-lsp-signature-help")
-  use("hrsh7th/cmp-vsnip")
-  use("hrsh7th/cmp-path")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/vim-vsnip")
+    -- Completion
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-nvim-lua")
+    use("hrsh7th/cmp-nvim-lsp-signature-help")
+    use("hrsh7th/cmp-vsnip")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/vim-vsnip")
 
-  -- File Explorer
-  use("nvim-tree/nvim-tree.lua")
-  use("nvim-tree/nvim-web-devicons")
+    -- File Explorer
+    use("nvim-tree/nvim-tree.lua")
+    use("nvim-tree/nvim-web-devicons")
 
-  -- Autopairs
-  use({
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup {}
-    end
-  })
+    -- Autopairs
+    use({
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    })
 
-  -- Markdown preview
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    setup = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  })
+    -- Markdown preview
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    })
 
-  -- Wakatime
-  use("wakatime/vim-wakatime")
+    -- Wakatime
+    use("wakatime/vim-wakatime")
 end)
 
